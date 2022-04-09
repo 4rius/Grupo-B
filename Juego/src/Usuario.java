3a49630ba1d7af1faabef6abdbd55e67ac4f2050
Santiago Arias <santi.ari.pani@outlook.com>
2022-04-09 15:44:23 +0200

    Serialize

--- Juego/src/Usuario.java
+++ Juego/src/Usuario.java
@@ -1,16 +1,38 @@
 import Aplicación.Multiplex;
 import Aplicación.Operation;
 
+import java.io.*;
 import java.util.HashMap;
 
 public class Usuario extends Operation {
-    private HashMap<String, Cliente> Clientes;
-    private HashMap<String, Operador> Operadores;
+    private static HashMap<String, Cliente> Clientes;
+    private static HashMap<String, Operador> Operadores;
 
-    public Usuario(Multiplex multiplex, HashMap<String, Cliente> clientes, HashMap<String, Operador> operadores) {
+    public Usuario(Multiplex multiplex, HashMap<String, Cliente> clientes, HashMap<String, Operador> operadores) throws IOException, ClassNotFoundException {
         super(multiplex);
-        Clientes = clientes;
-        Operadores = operadores;
+
+        File f = new File("assets/estado.bin");
+        if(f.exists()){
+            Usuario.deserialize();
+        } else {
+            this.Clientes = new HashMap<String, Cliente>();
+            this.Operadores = new HashMap<String, Operador>();
+        }
+    }
+
+    private static void deserialize() throws IOException, ClassNotFoundException {
+        FileInputStream finputstream = new FileInputStream("assets/estado.bin");
+        ObjectInputStream inputstream = new ObjectInputStream(finputstream);
+        Usuario.Clientes = (HashMap<String, Cliente>) inputstream.readObject();
+        Usuario.Operadores = (HashMap<String, Operador>) inputstream.readObject();
+    }
+
+    public static void serialize() throws IOException {
+        FileOutputStream foutputstream = new FileOutputStream("assets/estado.bin");
+        ObjectOutputStream outputstream = new ObjectOutputStream(foutputstream);
+        outputstream.writeObject(Clientes);
+        outputstream.writeObject(Operadores);
+        outputstream.close();
     }
 
     public HashMap<String, Cliente> getClientes() {
