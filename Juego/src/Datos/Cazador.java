1e1f4fbdd45512804c42a44bf9b79355fb63852b
DiosYisus77 <jesusp.sanchez77@gmail.com>
2022-04-18 13:56:55 +0200

    defTotal Cazador

--- Juego/src/Datos/Cazador.java
+++ Juego/src/Datos/Cazador.java
@@ -3,7 +3,7 @@ package Datos;
 import java.io.IOException;
 import java.util.List;
 
-public class Cazador extends Personaje{
+public class Cazador extends Personaje {
     private int ptosvoluntad;
     private int edad;
 
@@ -26,11 +26,20 @@ public class Cazador extends Personaje{
     }
 
     @Override
-    public int atkTotal(){
+    public int atkTotal() {
         int atk = 0;
         atk = this.getPoder() + this.ptosvoluntad + this.talento.getAtq() +
                 this.getArmaActual1().getModataque() + this.getArmaActual2().getModataque() +
                 this.getArmaduraActual().getModataque();
         return atk;
+    }
+
+    @Override
+    public int defTotal() {
+        int def = 0;
+        def = this.getPoder() + this.ptosvoluntad + this.talento.getDfs() +
+                this.getArmaActual1().getModdef() + this.getArmaActual2().getModdef() +
+                this.getArmaduraActual().getModdef();
+        return def;
     }
 }
