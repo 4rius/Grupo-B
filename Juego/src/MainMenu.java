477858ef40f625e41cd89088ae98931a0c12e868
Santiago Arias <santi.ari.pani@outlook.com>
2022-04-18 20:13:15 +0200

    Arreglar MainMenu

--- Juego/src/MainMenu.java
+++ Juego/src/MainMenu.java
@@ -1,18 +1,19 @@
+import Datos.Personaje;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.io.Console.*;
 
 public class MainMenu extends Operation {
-    private final ArrayList<Operation> OperationList;
 
-    public MainMenu(Multiplex multiplex, ArrayList<Operation> operationList) {
+    public MainMenu(Multiplex multiplex) {
         super(multiplex);
-        OperationList = operationList;
     }
 
     @Override
-    public void doOperation() {
+    public void doOperation() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Bienvenido al competidor del mismisimo The Witcher");
         System.out.println("1. Iniciar sesión");
@@ -20,41 +21,176 @@ public class MainMenu extends Operation {
         System.out.println("3. Salir");
         System.out.println("Introduzca una opción: ");
         int opcion = 0;
-        try { //Se hace asi porque es heredada de Operation
-            opcion = Integer.parseInt(br.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        opcion = Integer.parseInt(br.readLine());
         switch (opcion) {
-            case 1 -> iniciarSesion();
-            case 2 -> registrarse();
+            case 1 -> {
+                try {
+                    iniciarSesion();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            case 2 -> {
+                try {
+                    registrarse();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             case 3 -> System.out.println("Saliendo...");
             default -> {
-                System.out.println("Eso no es una opción válida");
+                System.out.println("Esa no es una opción válida");
                 doOperation();
             }
         }
 
     }
 
-    private void registrarse() {
+    private void registrarse() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Se va a registrar un operador (1) o un jugador (2)");
+        int opcion = 0;
+        opcion = Integer.parseInt(br.readLine());
+        switch (opcion) {
+            case 1 -> registrarOperador();
+            case 2 -> registrarJugador();
+        }
     }
 
-    private void iniciarSesion() {
-    }
-
-    public void mainMenu(String nick){
-        if (nick.equals("null")) {
-            System.out.println("Ocurrió un problema con su usuario/contraseña");
-            this.doOperation();
-        } else {
-            System.out.println("Bienvenido " + nick);
-            if (Multiplex.getNumerosderegistro().contains(nick)) {  //Si es un cliente
-                //Imprimir todas las operaciones marcadas como cliente
-            } else { //Si es un operador
-                //Imprimir todas las operaciones marcadas como operador
+    private void registrarJugador() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String Letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("Introduzca su nombre: ");
+        String nombre = br.readLine();
+        System.out.println("Nick: ");
+        String nick = br.readLine();
+        while (Multiplex.getClientes().containsKey(nick)) {
+            System.out.println("Ese nick ya existe, introduzca otro: ");
+            nick = br.readLine();
+        }
+        System.out.println("Introduzca su contraseña: ");
+        String contrasena =br.readLine();
+        String registro = null;
+        while (Multiplex.getRegistros().contains(registro) || registro == null) { //LNNLL
+            StringBuilder sb = new StringBuilder();
+            sb.append((int) (Letras.length() * Math.random()));
+            for (int i = 0; i < 2; i++) {
+                sb.append(Math.random() * 9);
             }
+            for (int i = 0; i < 2; i++) {
+                sb.append(Letras.charAt((int) (Math.random() * Letras.length())));
+            }
+            registro = sb.toString();
+        }
 
+        System.out.println("Tu identificación de registro es: " + registro);
+        Multiplex.getClientes().put(nick, new Cliente(nombre, nick, registro, contrasena));
+        Multiplex.getRegistros().add(registro);
+        System.out.println("Registrado con éxito, bienvenido, " + nick + "! Ya puedes iniciar sesión");
+        Multiplex.serialize();
+        this.doOperation();
+    }
+
+    private void registrarOperador() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Introduzca su nombre: ");
+        String nombre = br.readLine();
+        System.out.println("Nick: ");
+        String nick = br.readLine();
+        while (Multiplex.getClientes().containsKey(nick) || Multiplex.getOperadores().containsKey(nick)) {
+            System.out.println("Ese nick ya existe, introduzca otro: ");
+            nick = br.readLine();
+        }
+        System.out.println("Introduzca su contraseña: ");
+        String contrasena = br.readLine();
+
+        Multiplex.getOperadores().put(nick, new Operador(this.getMultiplex(), nombre, nick, contrasena));
+        System.out.println("Registrado con éxito, bienvenido, " + nick + "! Ya puedes iniciar sesión");
+        Multiplex.serialize();
+        this.doOperation();
+    }
+
+    private void iniciarSesion() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Introduzca su nick: ");
+        String nick = br.readLine();
+        System.out.println("Introduzca su contraseña: ");
+        String contrasena = br.readLine();
+        if (Multiplex.getClientes().containsKey(nick)) {
+            Cliente cliente = Multiplex.getClientes().get(nick);
+            if (cliente.getPassword().equals(contrasena)) {
+                this.mainMenu(1, nick);
+            } else {
+                System.out.println("Contraseña incorrecta");
+            }
+        } else if (Multiplex.getOperadores().containsKey(nick)) {
+            Operador operador = Multiplex.getOperadores().get(nick);
+            if (operador.getContraseña().equals(contrasena)) {
+                this.mainMenu(2, nick);
+            } else {
+                System.out.println("Contraseña incorrecta");
+            }
+        } else {
+            System.out.println("Ese usuario no existe");
+        }
+    }
+
+    public void mainMenu(int tipo, String nick) throws IOException {
+        System.out.println("Bienvenido " + nick);
+        if (tipo == 1) {  //Si es un cliente
+            System.out.println("1. Registrar personaje");
+            System.out.println("2. Eliminar el personaje actual");
+            System.out.println("3. Seleccionar equipo");
+            System.out.println("4. Crear desafío");
+            System.out.println("5. Ver desafíos pendientes");
+            System.out.println("6. Ver historial de combates");
+            System.out.println("7. Suscribirse a resultados");
+            System.out.println("8. Cerrar sesión");
+            System.out.println("Introduzca una opción: ");
+            int opcion = 0;
+            opcion = Integer.parseInt(System.console().readLine());
+            switch (opcion) {
+                case 1 -> Multiplex.getClientes().get(nick).registrarPersonaje();
+                case 2 -> {
+                    Multiplex.getClientes().get(nick).setPersonaje(null);
+                    System.out.println("Personaje eliminado");
+                }
+                case 3 -> Multiplex.getClientes().get(nick).seleccionarEquipo();
+                case 4 -> Multiplex.getClientes().get(nick).crearDesafio();
+                case 5 -> Multiplex.getClientes().get(nick).verDesafios();
+                case 6 -> Multiplex.getClientes().get(nick).verHistorial();
+                case 7 -> Multiplex.getClientes().get(nick).suscribirse();
+                case 8 -> this.doOperation();
+                default -> {
+                    System.out.println("Esa no es una opción válida");
+                }
+            }
+        } else { //Si es un operador
+            System.out.println("1. Editar personaje");
+            System.out.println("2. Eliminar personaje");
+            System.out.println("3. Editar equipo");
+            System.out.println("4. Editar modificador");
+            System.out.println("5. Editar esbirros");
+            System.out.println("5. Validar desafíos pendientes");
+            System.out.println("6. Banear jugador");
+            System.out.println("7. Desbanear jugador");
+            System.out.println("8. Cerrar sesión");
+            System.out.println("Introduzca una opción: ");
+            int opcion = 0;
+            opcion = Integer.parseInt(System.console().readLine());
+            switch (opcion) {
+                case 1 -> Multiplex.getOperadores().get(nick).editarPersonaje();
+                case 2 -> Multiplex.getOperadores().get(nick).eliminarPersonaje();
+                case 3 -> Multiplex.getOperadores().get(nick).editarEquipo();
+                case 4 -> Multiplex.getOperadores().get(nick).editarModificador();
+                case 5 -> Multiplex.getOperadores().get(nick).validarDesafios();
+                case 6 -> Multiplex.getOperadores().get(nick).banearJugador();
+                case 7 -> Multiplex.getOperadores().get(nick).desbanearJugador();
+                case 8 -> this.doOperation();
+                default -> {
+                    System.out.println("Esa no es una opción válida");
+                }
+            }
         }
     }
 }
