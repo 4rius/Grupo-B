fda43d7bb1a388747ee6c00b7cf9521fc4da3c8a
DiosYisus77 <jesusp.sanchez77@gmail.com>
2022-04-18 13:53:31 +0200

    defTotal Vampiro

--- Juego/src/Datos/Vampiro.java
+++ Juego/src/Datos/Vampiro.java
@@ -32,9 +32,21 @@ public class Vampiro extends Personaje{
                 this.getArmaActual1().getModataque() + this.getArmaActual2().getModataque() +
                 this.getArmaduraActual().getModataque();
         if (this.ptossangre >= 5){
-            ptossangre = ptossangre - this.disciplina.getCosteSangre();
             atk = atk + 2;
         }
         return atk;
+    }
+
+    @Override
+    public int defTotal(){
+        int def = 0;
+        def = this.getPoder() + this.disciplina.getDfs() +
+                this.getArmaActual1().getModdef() + this.getArmaActual2().getModdef() +
+                this.getArmaduraActual().getModdef();
+        if (this.ptossangre >= 5){
+            ptossangre = ptossangre - this.disciplina.getCosteSangre();
+            def = def + 2;
+        }
+        return def;
     }
 }
