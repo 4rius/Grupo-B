820904e6381ae4473b35e7324ccd26bef2a80690
DiosYisus77 <jesusp.sanchez77@gmail.com>
2022-04-18 13:45:27 +0200

    atkTotal licantropo

--- Juego/src/Datos/Licantropo.java
+++ Juego/src/Datos/Licantropo.java
@@ -4,13 +4,26 @@ import java.io.IOException;
 import java.util.List;
 
 public class Licantropo extends Personaje{
-    private int Rabia;
+    private int rabia;
+    private Don don;
 
     public int getRabia() {
-        return Rabia;
+        return rabia;
     }
 
     public void setRabia(int rabia) {
-        Rabia = rabia;
+        rabia = rabia;
+    }
+
+    @Override
+    public int atkTotal(){
+        int atk = 0;
+        atk = this.getPoder() + this.don.getAtq() +
+                this.getArmaActual1().getModataque() + getArmaActual2().getModataque() +
+                this.getArmaduraActual().getModataque();
+        if (this.rabia >= this.don.getMinRabia()){
+            atk = atk + this.rabia;
+        }
+        return atk;
     }
 }
